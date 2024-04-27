0f5448e8e3a30ef668bd67dcb83bb24dce9e37e9
rodrigovm10 <rv0611200369@gmail.com>
2024-04-27 00:19:27 -0600

    ⚙️ Config project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,36 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'selector',
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          50: '#fffaeb',
+          100: '#fff0c6',
+          200: '#ffdf88',
+          300: '#ffc84a',
+          400: '#ffb62e',
+          500: '#f98e07',
+          600: '#dd6802',
+          700: '#b74706',
+          800: '#94350c',
+          900: '#7a2c0d',
+          950: '#461502'
+        },
+        fontSize: {
+          xs: '0.75rem',
+          sm: '0.875rem',
+          base: '1rem',
+          lg: '1.125rem',
+          xl: '1.25rem',
+          '2xl': '1.5rem',
+          '3xl': '1.875rem',
+          '4xl': '2.25rem',
+          '5xl': '3rem',
+          '6xl': '4rem',
+          '7xl': '5rem'
+        }
+      }
+    }
   },
   plugins: []
 }
