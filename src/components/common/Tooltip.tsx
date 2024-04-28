98ad0fa3e37ca1f0faeb23e0f1765d5fa3080d5d
rodrigovm10 <rv0611200369@gmail.com>
2024-04-28 10:04:41 -0600

    🛠️ Fix header

--- src/components/common/Tooltip.tsx
+++ src/components/common/Tooltip.tsx
@@ -7,7 +7,7 @@ export function Tooltip({ children, tooltip }: TooltipProps) {
   return (
     <div className='group relative inline-block'>
       {children}
-      <span className='invisible group-hover:visible opacity-0 group-hover:opacity-100 transition-all bg-primary-500 dark:bg-primary-700 text-white p-1 rounded absolute top-full mt-2 text-[12px] text-center border border-primary-400 font-semibold'>
+      <span className='invisible group-hover:visible opacity-0 group-hover:opacity-100 transition-all bg-primary-500 dark:bg-primary-700 text-white p-1 rounded absolute top-full md:top-[65%] text-[12px] text-center border border-primary-400 font-semibold '>
         {tooltip}
       </span>
     </div>
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,11 +1,16 @@
-import { IconX, IconMenu2 } from '@tabler/icons-react'
 import { useState } from 'react'
-import { cn } from '@/libs/utils'
-import { Geolocation } from '@/components/header/Geolocation'
-import { SaveLocation } from '@/components/header/SaveLocation'
-import { Search } from '@/components/header/Search'
-import { Theme } from '@/components/header/Theme'
 import { useMediaQuery } from '@uidotdev/usehooks'
+
+import { cn } from '@/libs/utils'
+import { IconX, IconMenu2 } from '@tabler/icons-react'
+
+import { Theme } from '@/components/header/Theme'
+import { Search } from '@/components/header/Search'
+import { SaveLocation } from '@/components/header/SaveLocation'
+import { Geolocation } from '@/components/header/Geolocation'
+
+const date = new Date()
+const today = `Today, ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
 
 export function Header() {
   const [isOpen, setIsOpen] = useState(false)
@@ -18,14 +23,17 @@ export function Header() {
   return (
     <header
       className={cn(
-        'my-4 mx-2',
+        'w-full overflow-hidden p-6',
         !isSmallDevice && 'flex justify-between',
-        isSmallDevice && isOpen ? 'flex justify-center' : 'flex justify-between'
+        isSmallDevice && isOpen
+          ? 'flex justify-center border-b border-black/50 dark:border-white/50 mb-2'
+          : 'flex justify-between'
       )}
     >
-      <h3 className={cn(!isSmallDevice && 'block', isSmallDevice && isOpen ? 'hidden' : 'block')}>
-        weathr
-      </h3>
+      <div className={cn(!isSmallDevice && 'block', isSmallDevice && isOpen ? 'hidden' : 'block')}>
+        <h3 className='text-xl font-bold'>weathr</h3>
+        <span className='text-sm opacity-60'>{today}</span>
+      </div>
       <nav className='flex gap-x-2 '>
         <Geolocation className={cn('hidden md:block', isOpen && 'block')} />
         <SaveLocation className={cn('hidden md:block', isOpen && 'block')} />
