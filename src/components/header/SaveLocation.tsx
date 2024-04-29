1e1e29acf6e7d97c51834ca8888ace02d1848f5b
rodrigovm10 <rv0611200369@gmail.com>
2024-04-28 22:57:34 -0600

    🛠️ Sidebar width fixed

--- src/components/header/SaveLocation.tsx
+++ src/components/header/SaveLocation.tsx
@@ -27,7 +27,7 @@ export function SaveLocation({ className }: { className: string }) {
           <h3 className='font-normal text-lg mt-5'>Locations Saved</h3>
           <section className='text-center mt-10'>
             <IconDeviceFloppy className='mx-auto size-8 mb-2' />
-            <p className='text-sm'>All the locations you save will appear here</p>
+            <p className='text-sm text-pretty'>All the locations you save will appear here</p>
           </section>
           <Button
             icon={<IconX className='size-4 self-center' />}
